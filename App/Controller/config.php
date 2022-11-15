2dc570610239c71f8fb449c195c2b26bac25b53c
naldi1800 <nalditampan23@gmail.com>
2022-11-15 02:37:45 -0800

    v4

--- App/Controller/config.php
+++ App/Controller/config.php
@@ -4,7 +4,7 @@
     define("DB_HOST", "localhost");
     define("DB_USER", "root");
     define("DB_PASS", "");
-    define("DB_NAME", "apotek_safira_192111");
+    define("DB_NAME", "apotek_192111_safira");
 
     $link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
---
+++ App/Model/Mid.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace App\Model;
+
+use App\Contorller\Alert;
+use App\Model\Data;
+use mysqli;
+
+class Mid extends Data
+{
+
+    public static function GetAll($link)
+    {
+        $sql = "SELECT * FROM " . parent::$t_mid;
+        $query = mysqli_query($link, $sql);
+        $data = null;
+        while ($result = mysqli_fetch_array($query)) {
+            $data[] = $result;
+        }
+
+        return $data;
+    }
+
+    public static function GetWithId($link, $id)
+    {
+        $sql = "SELECT * FROM " . parent::$t_mid . " WHERE id_mid='" . $id . "'";
+        $query = mysqli_query($link, $sql);
+        return mysqli_fetch_assoc($query);
+    }
+
+    public static function Update($link, $id, $data)
+    {
+        $sql = "UPDATE " . parent::$t_mid . " SET "
+            . "Pasien='" . $data['Pasien'] . "' WHERE id_mid='" . $id . "'";
+
+        $query = mysqli_query($link, $sql);
+        if ($query) {
+            Alert::Set("Pasien", "diubah", "berhasil");
+        } else {
+            Alert::Set("Pasien", "diubah", "gagal");
+//            echo "Error : " . mysqli_error($link);
+        }
+    }
+
+    public static function Delete($link, $id)
+    {
+        $sql = "DELETE FROM " . parent::$t_mid . " WHERE id_mid='" . $id . "'";
+        $query = mysqli_query($link, $sql);
+        if ($query) {
+            Alert::Set("Pasien", "dihapus", "berhasil");
+        } else {
+            Alert::Set("Pasien", "dihapus", "gagal");
+        }
+    }
+
+    public static function Insert($link, $data)
+    {
+        $sql = "INSERT INTO " . parent::$t_mid . " VALUES( null, '"
+            . $data['Pasien'] . "','"
+            . $data['Keluhan'] . "')";
+            
+        
+        // var_dump($sql);
+        $query = mysqli_query($link, $sql);
+        
+        if ($query) {
+            Alert::Set("Pasien", "disimpan", "berhasil");
+        } else {
+            Alert::Set("Pasien", "disimpan", "gagal");
+        //    echo "Error : " . mysqli_error($link);
+        }
+    }
+}
